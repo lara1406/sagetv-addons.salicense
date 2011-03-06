99f130bee6fe2427ed56fcbc373df25ac9fd2d33
derek@battams.ca <derek@battams.ca@4d529e3f-d655-0410-97be-dbb2a4780eb3>
2011-03-06 16:23:48 +0000

    Validate location of license file path on the server before accepting the value
    git-svn-id: https://sagetv-addons.googlecode.com/svn/trunk/salicense@1420 4d529e3f-d655-0410-97be-dbb2a4780eb3

--- src/com/google/code/sagetvaddons/license/Plugin.java
+++ src/com/google/code/sagetvaddons/license/Plugin.java
@@ -19,7 +19,9 @@ import org.apache.log4j.PropertyConfigurator;
 
 import sage.SageTVPlugin;
 import sage.SageTVPluginRegistry;
+import sagex.api.Utility;
 import sagex.plugin.AbstractPlugin;
+import sagex.plugin.IPropertyValidator;
 import sagex.plugin.PluginProperty;
 import sagex.plugin.ServerPropertyPersistence;
 
@@ -36,13 +38,23 @@ public final class Plugin extends AbstractPlugin {
 		PropertyConfigurator.configure("plugins/salicense/salicense.log4j.properties");
 	}
 
+	static private class LicenseFileValidator implements IPropertyValidator {
+
+		@Override
+		public void validate(String prop, String val) throws Exception {
+			if(prop.equals(PROP_FILE) && !Utility.IsFilePath(val))
+				throw new IllegalArgumentException("File '" + val + "' does not exist on the server!");
+		}		
+	}
+	
 	public Plugin(SageTVPluginRegistry registry) {
 		super(registry);
 		PluginProperty prop = new PluginProperty(SageTVPlugin.CONFIG_TEXT, PROP_EMAIL, "", "Registered Email Address", "The email address associated with your sagetv-addons license file.  Changes to this value are immediate.");
 		prop.setPersistence(new ServerPropertyPersistence());
 		addProperty(prop);
-		prop = new PluginProperty(SageTVPlugin.CONFIG_FILE, PROP_FILE, "", "Location of License File", "The location of your sagetv-addons license file.  Changes to this value are immediate.");
+		prop = new PluginProperty(SageTVPlugin.CONFIG_FILE, PROP_FILE, "", "Location of License File", "The location of your sagetv-addons license file.  The file path is on your SageTV server.  You probably DO NOT want to change this from a SageClient installation.  Changes to this value are immediate.");
 		prop.setPersistence(new ServerPropertyPersistence());
+		prop.setValidator(new LicenseFileValidator());
 		addProperty(prop);
 	}
 }
