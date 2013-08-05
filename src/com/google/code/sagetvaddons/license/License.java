e8301cb9607246e7cf79757de19f8621b67ee1dc
Derek Battams <derek@battams.ca>
2013-08-05 00:55:53 -0400

    Remove lic file validation; it's free lifetime licenses for everyone!

--- src/com/google/code/sagetvaddons/license/License.java
+++ src/com/google/code/sagetvaddons/license/License.java
@@ -1,5 +1,5 @@
 /*
- *      Copyright 2011 Battams, Derek
+ *      Copyright 2011-2013 Battams, Derek
  *       
  *       Licensed under the Apache License, Version 2.0 (the "License");
  *       you may not use this file except in compliance with the License.
@@ -15,166 +15,27 @@
  */
 package com.google.code.sagetvaddons.license;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.StringReader;
-import java.math.BigInteger;
-import java.nio.charset.Charset;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.spec.RSAPrivateKeySpec;
-import java.util.Date;
-import java.util.Properties;
-
-import javax.crypto.Cipher;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.log4j.Logger;
-
-import sagex.api.Configuration;
-import sagex.api.Global;
-import sagex.api.PluginAPI;
-import sagex.api.Utility;
 
 /**
  * @author dbattams
  *
  */
 public final class License {
-	static private final Logger LOG = Logger.getLogger(License.class);
-	static private final String EMAIL_PROP = "mail";
-	static private final long PURCHASED_TS = -1L;
-	static private final String LIFETIME_ID = "lifetime";
+	static private final LicenseResponse FREE_LICENSE = new License().isLicensed();
 
 	static public LicenseResponse isLicensed(String pluginId) {
-		LOG.info("Received request from '" + pluginId + "' for plugin '" + pluginId + "'");
-		return new License(pluginId).isLicensed();
+		return FREE_LICENSE;
 	}
 
-	static public void autoConfig(String email, String filePath) {
-		if(email != null && email.length() > 0 && "".equals(Configuration.GetServerProperty(Plugin.PROP_EMAIL, "")))
-			Configuration.SetServerProperty(Plugin.PROP_EMAIL, email);
-		if(filePath != null && filePath.length() > 0 && "".equals(Configuration.GetServerProperty(Plugin.PROP_FILE, "")))
-			Configuration.SetServerProperty(Plugin.PROP_FILE, filePath);
-	}
+	final private LicenseResponse resp;
 
-	static private boolean isPluginEnabled() {
-		Object[] installed = Global.IsClient() ? PluginAPI.GetInstalledClientPlugins() : PluginAPI.GetInstalledPlugins();
-		for(Object plugin : installed) {
-			if(PluginAPI.GetPluginIdentifier(plugin).equals(Plugin.PLUGIN_ID))
-				return PluginAPI.IsPluginEnabled(plugin);
-		}
-		return false;
-	}
-
-	private PrivateKey key;
-	private LicenseResponse resp;
-	private String registeredEmail, requestor, filePath;
-	private Properties props;
-
-	private License(String requestor) {
-		this.requestor = requestor;
-		filePath = Configuration.GetServerProperty(Plugin.PROP_FILE, "");
-		registeredEmail = Configuration.GetServerProperty(Plugin.PROP_EMAIL, "");
+	private License() {
 		resp = new LicenseResponse();
-		props = null;
-		try {
-			getPayload();
-		} catch(Exception e) {
-			props = new Properties();
-		}
 	}
 
 	public LicenseResponse isLicensed() {
-		if(!isPluginEnabled()) {
-			String err = "sagetev-addons license plugin is not enabled!  Enable the plugin.";
-			LOG.warn(requestor + ": " + err);
-			resp.setLicensed(false);
-			resp.setMessage(err);
-		} else if(filePath.length() == 0) {
-			String err = "No license file specified!  Please configure the sagetv-addons license plugin.";
-			LOG.warn(requestor + ": " + err);
-			resp.setLicensed(false);
-			resp.setMessage(err);
-		} else if(!Utility.IsFilePath(filePath)) {
-			String err = "Cannot read specified license file on the server! [" + filePath + "]";
-			LOG.warn(requestor + ": " + err);
-			resp.setLicensed(false);
-			resp.setMessage(err);
-		} else if(registeredEmail.length() == 0) {
-			String err = "Registered email not specified!  Please configure the sagetv-addons license plugin.";
-			resp.setLicensed(false);
-			resp.setMessage(err);
-		} else {
-			String licensedEmail = getLicensedEmail();
-			if(licensedEmail == null || !registeredEmail.toLowerCase().equals(licensedEmail.toLowerCase())) {
-				String err = "The registered email ['" + registeredEmail + "'] does not match the email in the license file!";
-				LOG.warn(requestor + ": " + err);
-				resp.setLicensed(false);
-				resp.setMessage(err);
-			} else if(!isPluginLicenseValid(requestor) && !isPluginLicenseValid(LIFETIME_ID)) {
-				String err = "The license file is not entitled to plugin id '" + requestor + "'";
-				LOG.warn(err);
-				resp.setLicensed(false);
-				resp.setMessage(err);
-			} else {
-				resp.setLicensed(true);
-				resp.setMessage("OK");
-				LOG.info(requestor + ": License verified successfully!");
-			}
-		}
+		resp.setLicensed(true);
+		resp.setMessage("OK");
 		return resp;
-	}
-
-	private void getPayload() throws Exception {
-		String data = Utility.GetFileAsString(new File(filePath));
-		if(data == null || data.length() == 0) {
-			LOG.error("License file is empty! [" + filePath + "]");
-			return;
-		}
-		initKey();
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.DECRYPT_MODE, key);
-		Charset utf8 = Charset.forName("UTF-8");
-		String propsData = new String(cipher.doFinal(Base64.decodeBase64(data.getBytes(utf8))), utf8);
-		Properties props = new Properties();
-		props.load(new StringReader(propsData));
-		this.props = props;
-	}
-
-	private String getLicensedEmail() {
-		return props.getProperty(EMAIL_PROP);
-	}
-
-	private boolean isPluginLicenseValid(String pluginId) {
-		String ts = props.getProperty(pluginId.toLowerCase());
-
-		if(ts == null || ts.length() == 0 || (!ts.equals(Long.toString(PURCHASED_TS)) && !ts.matches("\\d+")))
-			return false;
-		long expiry = Long.parseLong(ts);
-		return expiry == PURCHASED_TS || new Date(expiry).after(new Date());
-	}
-
-	private void initKey() throws Exception {
-		InputStream privateKey = License.class.getResourceAsStream("/com/google/code/sagetvaddons/license/sagetv-addons.private");
-		if(privateKey != null) {
-			ObjectInputStream oin = null;
-			BigInteger mod, exp;
-			try {
-				oin = new ObjectInputStream(new BufferedInputStream(privateKey));
-				mod = (BigInteger) oin.readObject();
-				exp = (BigInteger) oin.readObject();
-				RSAPrivateKeySpec keySpec = new RSAPrivateKeySpec(mod, exp);
-				KeyFactory fact = KeyFactory.getInstance("RSA");
-				key = fact.generatePrivate(keySpec);
-			} finally {
-				if(oin != null)
-					try { oin.close(); } catch(IOException e) { LOG.error("IOError", e); }
-			}
-		} else
-			throw new RuntimeException("Unable to find public key file!");
 	}
 }
--- src/com/google/code/sagetvaddons/license/Plugin.java
+++ src/com/google/code/sagetvaddons/license/Plugin.java
@@ -1,5 +1,5 @@
 /*
- *      Copyright 2011 Battams, Derek
+ *      Copyright 2011-2013 Battams, Derek
  *       
  *       Licensed under the Apache License, Version 2.0 (the "License");
  *       you may not use this file except in compliance with the License.
@@ -15,16 +15,8 @@
  */
 package com.google.code.sagetvaddons.license;
 
-import org.apache.log4j.PropertyConfigurator;
-
-import sage.SageTVPlugin;
 import sage.SageTVPluginRegistry;
-import sagex.api.Global;
-import sagex.api.Utility;
 import sagex.plugin.AbstractPlugin;
-import sagex.plugin.IPropertyValidator;
-import sagex.plugin.PluginProperty;
-import sagex.plugin.ServerPropertyPersistence;
 
 /**
  * @author dbattams
@@ -32,32 +24,8 @@ import sagex.plugin.ServerPropertyPersistence;
  */
 public final class Plugin extends AbstractPlugin {
 	static final String PLUGIN_ID = "salicense";
-	static final String PROP_EMAIL = "sagetvaddons/license/email";
-	static final String PROP_FILE = "sagetvaddons/licesne/file";
-
-	static {
-		PropertyConfigurator.configure("plugins/salicense/salicense.log4j.properties");
-	}
-
-	static private class LicenseFileValidator implements IPropertyValidator {
-
-		@Override
-		public void validate(String prop, String val) throws Exception {
-			if(prop.equals(PROP_FILE) && !Utility.IsFilePath(val))
-				throw new IllegalArgumentException("File '" + val + "' does not exist on the server!");
-		}		
-	}
 
 	public Plugin(SageTVPluginRegistry registry) {
 		super(registry);
-		if(!Global.IsClient()) {
-			PluginProperty prop = new PluginProperty(SageTVPlugin.CONFIG_TEXT, PROP_EMAIL, "", "Registered Email Address", "The email address associated with your sagetv-addons license file.  Changes to this value are immediate.");
-			prop.setPersistence(new ServerPropertyPersistence());
-			addProperty(prop);
-			prop = new PluginProperty(SageTVPlugin.CONFIG_FILE, PROP_FILE, "", "Location of License File", "The location of your sagetv-addons license file.  The file path is on your SageTV server.  You probably DO NOT want to change this from a SageClient installation.  Changes to this value are immediate.");
-			prop.setPersistence(new ServerPropertyPersistence());
-			prop.setValidator(new LicenseFileValidator());
-			addProperty(prop);
-		}
 	}
 }
